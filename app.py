af858afffb10422ea5b2e43a9ad5c7e82c32aca8
Joey, K <40598758+naeyanika@users.noreply.github.com>
2025-01-20 11:12:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,13 +71,13 @@ def categorize_description(description, custom_keywords):
 
     # Cek Manajer
     manager_keywords = ['manager', 'manajer', 'branch manager', 'kepala cabang', 'mc', 'bm']
-    if is_similar(description, manager_keywords, threshold=95):
+    if is_similar(description, manager_keywords, threshold=90):
         return 'MANAGER'
         
     #Cek custom keywords
     for category, keywords in custom_keywords.items():
         if keywords:
-            if is_similar(description, keywords, threshold=80):
+            if is_similar(description, keywords, threshold=100):
                 return category
 
     # Cek LAINYA
